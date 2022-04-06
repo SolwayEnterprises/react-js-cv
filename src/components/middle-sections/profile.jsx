2628e78db1444163216a71a8c199d138a0d11fa4
TheLionIsAlive <daniel.casella18@gmail.com>
2022-04-06 03:18:51 -0500

    update listening port

--- src/components/middle-sections/profile.jsx
+++ src/components/middle-sections/profile.jsx
@@ -7,8 +7,9 @@ export default function Profile() {
         <h2>Here's what I'm about.</h2>
         <p className="bio">
           I love empowering individuals and groups who seek to honor the
-          Creator through what they do. I seek to use remote work in
-          programming as a vehicle for self-supported mission work.
+          Creator through what they do. Through this brand, I seek to use
+          remote work in programming as a vehicle to support community
+          development effort on the side.
         </p>
       </div>);
 }
